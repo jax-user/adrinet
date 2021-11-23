4c851735730dba0e5259834fdb80a65ace1f75cc
Andrew <85534243+jax-user@users.noreply.github.com>
2021-11-23 13:48:37 +0000

    Made data cleaning tutorial

--- src/data/us10ybond_andrew.py
+++ src/data/us10ybond_andrew.py
@@ -1,7 +1,11 @@
 from datetime import date
 import investpy
 
-search_result = investpy.bonds.get_bond_historical_data('United States 10-Year', '1/1/1000', date.today().strftime('%d/%m/%Y'), order='descending')
-
+search_result = investpy.search_quotes(text='us10', products=['bonds'],
+                                       countries=['united states'], n_results=1)
 print(search_result)
+
+
+data = search_result.retrieve_historical_data(from_date='1/1/', to_date=date.today().strftime('%d/%m/%Y'))
+print(data.head())
 
