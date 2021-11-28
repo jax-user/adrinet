13a8bd90e488c420a92c942ec8d15f7e69d6e49a
ReiYamahara202105260 <rei.yamahara@gmail.com>
2021-11-28 19:44:39 +0000

    gold data

---
+++ src/data/gold_rei.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import yfinance as yf
+from sklearn import preprocessing
+
+gold = yf.Ticker('GC=F')
+gold_data = gold.history(period = 'max')
+gold_data = gold_data.drop(['Volume', 'Dividends', 'Stock Splits'], axis = 1)
+gold_data = gold_data.pct_change()[1:]
+
+for column in gold_data: 
+    mean = np.mean(gold_data[column])
+    std = np.std(gold_data[column])
+    gold_data[column] = (gold_data[column] - mean)/std
+
+scalar = preprocessing.MinMaxScaler()
+gold_data[['Open', 'High', 'Low', 'Close']] = scalar.fit_transform(gold_data[["Open", "High", "Low", "Close"]])
+gold_data.to_csv('data/cleaned/gold_cleaned.csv')
